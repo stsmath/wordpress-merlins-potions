ae1357bc291c44bba760c1d27986997e128beb33
Matheus Amaral <spn.matheus@gmail.com>
2025-06-12 16:53:27 -0300

    feat: finalizando footer

--- wp-content/themes/wordpress-merlins-potions/footer.php
+++ wp-content/themes/wordpress-merlins-potions/footer.php
@@ -1,7 +1,48 @@
-<footer>
-  <div class="">
-    <div class="">
-      <p class="">Merlin's Potions <?php echo date("Y") ?> - Alguns direitos reservados.</p>
+<footer class="m-footer">
+  <?php $footer = get_post('36') ?>
+  <div class="m-footer-container">
+    <div class="m-footer-icons-direitos">
+      <div class="m-footer-icons-utils">
+        <a class="links-utils" href="<?php the_field('link_contato_1', $footer); ?>">
+          <?php the_field('texto_contato_1', $footer); ?>
+        </a>
+        <a class="links-utils" href="<?php the_field('link_contato_2', $footer); ?>">
+          <?php the_field('texto_contato_2', $footer); ?>
+        </a>
+        <a class="links-utils" href="<?php the_field('link_contato_3', $footer); ?>">
+          <?php the_field('texto_contato_3', $footer); ?>
+        </a>
+      </div>
+      <div class="m-footer-icons">
+        <a href="<?php the_field('link_primeiro_icone', $footer) ?>" class="icone-rede link-rede" target="_blank">
+          <img src="<?php the_field('primeiro_icone', $footer) ?>" alt="Icone rede social" />
+        </a>
+        <a href="<?php the_field('link_segundo_icone', $footer) ?>" class="icone-rede link-rede" target="_blank">
+          <img src="<?php the_field('segundo_icone', $footer) ?>" alt="Icone rede social" />
+        </a>
+        <a href="<?php the_field('link_terceiro_icone', $footer) ?>" class="icone-rede link-rede" target="_blank">
+          <img src="<?php the_field('terceiro_icone', $footer) ?>" alt="Icone rede social" />
+        </a>
+        <a href="<?php the_field('link_quarto_icone', $footer) ?>" class="icone-rede link-rede" target="_blank">
+          <img src="<?php the_field('quarto_icone', $footer) ?>" alt="Icone rede social" />
+        </a>
+        <a href="<?php the_field('link_quinto_icone', $footer) ?>" class="icone-rede link-rede" target="_blank">
+          <img src="<?php the_field('quinto_icone', $footer) ?>" alt="Icone rede social" />
+        </a>
+      </div>
+      <p class="m-footer-direitos">© <?php echo date("Y") ?> <?php bloginfo('title') ?>. All Rights Reserved</p>
+    </div>
+    <div class="m-footer-newsletter">
+      <p class="m-footer-newsletter-title">
+        Sign up for our Newsletter
+      </p>
+      <p class="m-footer-newsletter-text">
+        Sign up for our newsletter & get exclusives discounts!
+      </p>
+      <div class="m-footer-newsletter--subs">
+        <input type="text" class="input-newsletter" id="campoNewsletter" placeholder="Enter your email" />
+        <button class="botao-newsletter">Submit</button>
+      </div>
     </div>
   </div>
 </footer>
--- wp-content/themes/wordpress-merlins-potions/functions.php
+++ wp-content/themes/wordpress-merlins-potions/functions.php
@@ -27,7 +27,7 @@ function wp_merlins_scripts()
   wp_enqueue_script('wp-merlins-scripts');
 
 }
-add_action('wp_enqueue_scripts', 'wp_merlins_scripts');
+// add_action('wp_enqueue_scripts', 'wp_merlins_scripts');
 
 function wp_merlins_style()
 {
@@ -73,4 +73,77 @@ function custom_post_type_cms_header()
 add_action('init', 'custom_post_type_cms_header');
 
 //Página com informações gerais do Footer.
+
+function custom_post_type_cms_footer()
+{
+  register_post_type('Custom Elements Footer', array(
+    'label' => 'Custom Elements Footer',
+    'description' => 'Informações de Campos personalizados do Footer',
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'capability_type' => 'post',
+    'map_meta_cap' => true,
+    'hierarchical' => false,
+    'exclude_from_search' => true,
+    'rewrite' => array('slug' => 'Custom Elements Footer', 'with_front' => true),
+    'query_var' => true,
+    'supports' => array('title', 'editor', 'page-attributes', 'post-formats'),
+
+    'labels' => array(
+      'name' => 'Custom Elements Footer',
+      'singular_name' => 'CMS Footer',
+      'menu_name' => 'CMS Footer',
+      'add_new' => 'Adicionar Novo',
+      'add_new_item' => 'Adicionar Novo CMS Footer',
+      'edit' => 'Editar',
+      'edit_item' => 'Editar CMS Footer',
+      'new_item' => 'Novo CMS Footer',
+      'view' => 'Ver CMS Footer',
+      'view_item' => 'Ver CMS Footer',
+      'search_items' => 'Procurar CMS Footer',
+      'not_found' => 'Nenhum CMS Footer Encontrado',
+      'not_found_in_trash' => 'Nenhum CMS Footer Encontrado no Lixo',
+    )
+  ));
+}
+
+add_action('init', 'custom_post_type_cms_footer');
+
+
+// Allow SVG
+add_filter('wp_check_filetype_and_ext', function ($data, $filename, $mimes) {
+
+  global $wp_version;
+  if ($wp_version !== '4.7.1') {
+    return $data;
+  }
+
+  $filetype = wp_check_filetype($filename, $mimes);
+
+  return [
+    'ext' => $filetype['ext'],
+    'type' => $filetype['type'],
+    'proper_filename' => $data['proper_filename']
+  ];
+
+}, 10, 4);
+
+function cc_mime_types($mimes)
+{
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
+function fix_svg()
+{
+  echo '<style type="text/css">
+        .attachment-266x266, .thumbnail img {
+             width: 100% !important;
+             height: auto !important;
+        }
+        </style>';
+}
+add_action('admin_head', 'fix_svg');
 ?>
